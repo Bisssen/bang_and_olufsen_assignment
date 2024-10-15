5fa881de4712e5d67eac827284257b760077e702
Bisssen <45271616+Bisssen@users.noreply.github.com>
2024-10-15 20:07:00 +0200

    Setup of basic layout

---
+++ api_interactor/api_interactor.py
@@ -0,0 +1,13 @@
+import requests as rq
+
+class api_interactor():
+    def __init__(self) -> None:
+        pass
+
+
+    def test(self) -> None:    
+    '''
+    1. Create a `Python` application (with v3.10 or higher) using Object Oriented Programming principles that makes use of type hints throughout the code. The application will interact with the JSONplaceholder API (https://jsonplaceholder.typicode.com/).
+    The application should be able to fetch and print data from the JSONplaceholder API. Specifically, it should fetch data from at least two different endpoints, such as `/posts` and `/users`.
+    '''
+    pass
---
+++ main.py
@@ -0,0 +1,7 @@
+from api_interactor.api_interactor import api_interactor
+
+
+
+if __name__ == "__main__":
+    tmp = api_interactor()
+    tmp.test()
