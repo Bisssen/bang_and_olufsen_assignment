16c59d1613d544e5b0161dd51b60c8454b0c5463
Bisssen <45271616+Bisssen@users.noreply.github.com>
2024-10-15 20:46:27 +0200

    Added fetch all users
    Still messy with commented unused code

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from api_interactor.api_interactor import api_interactor
 
 
 if __name__ == "__main__":
-    tmp = api_interactor()
-    tmp.test()
+    tmp = api_interactor(['users', 'posts'])
+    tmp.loop()
